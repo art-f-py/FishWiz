e65f8d8d670857f829353d5f014f447a8497f5a4
Artur <7arturfeijopoa@gmail.com>
2026-02-23 20:27:36 -0300

    v2.0 - Select de estados, novas espécies e imagens

--- js/script.js
+++ js/script.js
@@ -1,39 +1,67 @@
-function carregarEspecies() {
-    const select = document.getElementById('idespecie')
-    select.innerHTML = ""
+function carregarEstados() {
+    let selectEstados = document.getElementById('idestado')
+    selectEstados.innerHTML = ""
 
     const optionPadrao = document.createElement('option')
     optionPadrao.value = ""
-    optionPadrao.textContent = "Selecione uma espécie"
-    select.appendChild(optionPadrao)
+    optionPadrao.text = "Selecione o estado"
+    selectEstados.appendChild(optionPadrao)
 
-    const nomes = Object.keys(especies)
-    
-    for (let chave in especies) {
+    for (let estado in estados) {
+        const option = document.createElement('option')
+        option.value = estado
+        option.text = (estados[estado].nome)
+        selectEstados.appendChild(option)
+    }
+}
+function carregarEspecies(estadoSelecionado) {
+    const selectEspecies = document.getElementById('idespecie')
+    selectEspecies.innerHTML = ""
+
+    const optionPadrao = document.createElement('option')
+    optionPadrao.value = ""
+    optionPadrao.text = "Selecione a espécie"
+    selectEspecies.appendChild(optionPadrao)
+
+    if (estadoSelecionado === "") {
+        return
+    }
+
+    const especiesdoEstado = estados[estadoSelecionado].especies
+
+    for (let chave in especiesdoEstado) {
         const option = document.createElement('option')
         option.value = chave
-        option.textContent = especies[chave].nome
-        select.appendChild(option)
+        option.textContent = especiesdoEstado[chave].nome
+        selectEspecies.appendChild(option)
     }
 }
+
+document.getElementById('idestado').addEventListener('change', function() {
+    const estadoSelecionado = this.value
+    carregarEspecies(estadoSelecionado)
+})
+
 function adicionar() {
-    let especieSelecionada = document.getElementById('idespecie').value
-    let resultado = document.getElementById('resultado')
+    const estadoSelecionado = document.getElementById('idestado').value
+    const especieSelecionada = document.getElementById('idespecie').value
 
-    if (especieSelecionada === "") {
-        window.alert("[ERRO] Confira os dados e tente novamente.")
-    } else {
-        let peixe = especies[especieSelecionada]
+    if (estadoSelecionado === "" || especieSelecionada === "") {
+        window.alert("[ERRO] Confira as informações e tente novamente.")
+        return
+} else {
+    const peixe = estados[estadoSelecionado].especies[especieSelecionada]
+    resultado.innerHTML = `
+        <h3>${peixe.nome}</h3>
+        <img src="${peixe.imagem}" alt="${peixe.nome}" style="max-width:400px; display:block; margin-bottom:10px;">
+        <p><strong>Ambiente:</strong> ${peixe.ambiente}</p>
+        <p><strong>Isca:</strong> ${peixe.isca}</p>
+        <p><strong>Vara:</strong> ${peixe.vara}</p>
+        <p><strong>Linha:</strong> ${peixe.linha}</p>
+        <p><strong>Caster:</strong> ${peixe.caster}</p>
+    `
+}}
 
-        resultado.innerHTML = `
-            <h3>${peixe.nome}</h3>
-            <p><strong>Ambiente:</strong> ${peixe.ambiente}</p>
-            <p><strong>Isca:</strong> ${peixe.isca}</p>
-            <p><strong>Vara:</strong> ${peixe.vara}</p>
-            <p><strong>Linha:</strong> ${peixe.linha}</p>
-            <p><strong>Caster:</strong> ${peixe.caster}</p>
-        `
-    }
+window.onload = function() {
+    carregarEstados()
 }
-
-window.onload = carregarEspecies
