4db4533161211b5cf83bff93194ca9fbc61d4181
Artur <7arturfeijopoa@gmail.com>
2026-02-23 16:05:56 -0300

    v1.1 - Select de espécies gerado dinamicamente

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+function carregarEspecies() {
+    const select = document.getElementById('idespecie')
+    select.innerHTML = ""
+
+    const optionPadrao = document.createElement('option')
+    optionPadrao.value = ""
+    optionPadrao.textContent = "Selecione uma espécie"
+    select.appendChild(optionPadrao)
+
+    const nomes = Object.keys(especies)
+    
+    for (let chave in especies) {
+        const option = document.createElement('option')
+        option.value = chave
+        option.textContent = especies[chave].nome
+        select.appendChild(option)
+    }
+}
 function adicionar() {
     let especieSelecionada = document.getElementById('idespecie').value
     let resultado = document.getElementById('resultado')
 
-    if (especieSelecionada == "") {
+    if (especieSelecionada === "") {
         window.alert("[ERRO] Confira os dados e tente novamente.")
     } else {
         let peixe = especies[especieSelecionada]
@@ -17,3 +35,5 @@ function adicionar() {
         `
     }
 }
+
+window.onload = carregarEspecies
